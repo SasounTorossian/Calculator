618014c22c98fe51faa334b5e245a6be5f51aa54
SasounTorossian <46853885+SasounTorossian@users.noreply.github.com>
2020-06-24 14:56:38 +0100

    Moved all keyboard checking into single function

--- scripts/script.js
+++ scripts/script.js
@@ -14,14 +14,58 @@ const operators = [...document.querySelectorAll(".operator")].map(operator => op
 let outputID = document.getElementById("displayOutput") // Output handle
 outputID.textContent = "0"
 
+// Click event handler for numeric operands 0-9
+let operandContainer = document.querySelectorAll(".operand")
+operandContainer.forEach(operand => {
+	operand.addEventListener('click', e => appendOperand(e.target.value), false)
+})
 
-let operandFunc = function(e) {
-	let operand
+// Click event handler for sign operators "+", "-", "*", "/"
+let signOperatorContainer = document.querySelectorAll(".signOperator")
+signOperatorContainer.forEach(signOperator => {
+	signOperator.addEventListener('click', e => appendSignOperator(e.target.value), false)
+})
 
-	if(e.type == 'keydown' && isFinite(parseInt(e.key))) operand = e.key
-	else if(e.type == 'keydown' && e.key == '.') operand = e.key
-	else if (e.type == 'click') operand = e.target.value
-	else return
+// Click event handler called for operators "(", ")", "Back", and "Clear"
+let operatorContainer = document.querySelectorAll(".operator")
+	operatorContainer.forEach(operator => {
+		operator.addEventListener('click', e => appendOperator(e.target.value), false) 
+})
+
+// Keyboard event handler for numeric operands 0-9
+window.addEventListener('keydown', checkKey, false)
+
+function checkKey(e) {
+
+	let key = e.key
+	
+	let keyRegex = /^[0-9\.\*\/\-\+x\=c]|Enter|Backspace|Delete/i;
+	if(keyRegex.test(key)) {
+		console.log(key)
+		if (key == 'Enter' || key == '=' || key == "+" || key == "-" || key == "*" || key == "/") {
+			if(key == 'Enter') key = "="
+			if(key == "*" || key == "x") key = "x"
+			console.log("appendSignOperator")
+			appendSignOperator(key)
+		}
+		else if (key == 'c' || key == "Backspace" || key == "Delete") {
+			
+			if(key == 'c') key = "Clear"
+			if(key == "Backspace" || key == "Delete") key = "Back"
+			console.log("appendOperator")
+			appendOperator(key)
+		}
+		else {
+			console.log("appendOperand")
+			appendOperand(key)
+		}
+		
+
+	}
+}
+
+function appendOperand(e) {
+	let operand = e
 
 	if (outputID.textContent == "") {
 		outputID.textContent = "0"
@@ -55,148 +99,110 @@ let operandFunc = function(e) {
 	this.blur(); // Removes focus from buttons
 }
 
-// Click event handler for numeric operands 0-9
-let operandContainer = document.querySelectorAll(".operand")
-operandContainer.forEach(operand => {
-	operand.addEventListener('click', operandFunc, false)
-})
+function appendSignOperator(e){
+let operator = e
 
-// Keyboard event handler for numeric operands 0-9
-window.addEventListener('keydown', operandFunc, false)
+switch (operator) {
+	case "=":
+		// Get expression as string from output text box
+		const exp = outputID.textContent
+		// Calc value based off string
+		let expCalc = operate(exp)
+		// Round value to 3 decimal places if needed
+		let expCalcRounded = roundToN(expCalc, 3)
 
-
-let signOperatorFunc = function(e) {
-	let operator
-	
-	if(e.type == 'keydown' && signOperators.indexOf(e.key) > -1) operator = e.key
-	else if (e.type == 'keydown' && e.key == '*') operator = "x"
-	else if (e.type == 'keydown' && e.key == 'Enter') operator = "="
-	else if (e.type == 'click') operator = e.target.value
-	else return
-
-	switch (operator) {
-		case "=":
-			// Get expression as string from output text box
-			const exp = outputID.textContent
-			// Calc value based off string
-			let expCalc = operate(exp)
-			// Round value to 3 decimal places if needed
-			let expCalcRounded = roundToN(expCalc, 3)
-
-			// If rounding truncates decimal value to zero (hence removing decimal) e.g. 0.00002 x 2 = 0, then allow decimals to be used again. 
-			if (!Number.isInteger(expCalc) && 
-				Number.isInteger(expCalcRounded)) {
-					decimalAllowed = true
-				}
-
-			// If divided by zero, send message to user
-			if (!isFinite(expCalcRounded)) {
-				outputID.textContent = errorMessage
-				firstInput = true
-				return
+		// If rounding truncates decimal value to zero (hence removing decimal) e.g. 0.00002 x 2 = 0, then allow decimals to be used again. 
+		if (!Number.isInteger(expCalc) && 
+			Number.isInteger(expCalcRounded)) {
+				decimalAllowed = true
 			}
-									
-			//If zero, treat result as first input
-			if (expCalcRounded == "0") firstInput = true
 
-			outputID.textContent = expCalcRounded
-			return
-
-		case "-":
-			// Allow minus sign to function as negative sign if last characters are not already sign operators
-			// console.log()
-			if(matchPreviousCharacters(-2, signOperators) ||
-				matchPreviousCharacters(-1, "-")) {
-				return
-			}
-			if (firstInput) outputID.textContent = operator
-			else outputID.textContent += operator
-			break 
-
-		default:
-			// If previous character is open bracket or sign operator, then do not accept sign input. Prevents scenarios such as 27(*5) and 8*/2
-			if (matchPreviousCharacters(-1, "(", signOperators) || 
-				(firstInput && outputID.textContent == errorMessage)) {
-				return
-			}
-			outputID.textContent += operator
-			break
-		}
-
-		firstInput = false
-		decimalAllowed = true
-		this.blur(); // Removes focus from buttons
-	}
-
-// Click event handler for sign operators "+", "-", "*", "/"
-let signOperatorContainer = document.querySelectorAll(".signOperator")
-signOperatorContainer.forEach(signOperator => {
-	signOperator.addEventListener('click', signOperatorFunc, false)
-})
-
-// Keyboard event handler for sign operators "+", "-", "*", "/"
-window.addEventListener('keydown', signOperatorFunc, false)
-
-
-let operatorFunc = function(e) {
-	let operator
-
-	if(e.type == 'keydown' && operators.indexOf(e.key) > -1) operator = e.key
-	else if(e.type == 'keydown' && (e.key == 'c' || e.key == 'C')) operator = "Clear"
-	else if(e.type == 'keydown' && e.key == "Backspace") operator = "Back"
-	else if (e.type == 'click') operator = e.target.value
-	else return
-
-	switch (operator) {
-		case "Back":
-			// If back, remove last element from output. Keeps track of opened brackets if there are any.
-			if (matchPreviousCharacters(-1, ")")) openedBrackets++
-			if (matchPreviousCharacters(-1, "(")) openedBrackets--
-			if (matchPreviousCharacters(-1, ".")) decimalAllowed = true
-			outputID.textContent = outputID.textContent.slice(0, -1)
-			break
-
-		case "Clear":
-			// If clear, erase display and allow decimals to be used
-			outputID.textContent = "0"
+		// If divided by zero, send message to user
+		if (!isFinite(expCalcRounded)) {
+			outputID.textContent = errorMessage
 			firstInput = true
-			decimalAllowed = true
-			openedBrackets = 0
 			return
+		}
+								
+		//If zero, treat result as first input
+		if (expCalcRounded == "0") firstInput = true
 
-		case "(":
-			// If open bracket, increment bracket counter
-			if (firstInput) outputID.textContent = operator
-			else outputID.textContent += operator
-			openedBrackets++
-			break
+		outputID.textContent = expCalcRounded
+		return
 
-		case ")":
-			// If close bracket, make sure open bracket, sign operator or open bracket/decimal does not immediately preceed it. Avoids scenarios like "()", "45+(2+)", and "(.)"
-			if (!openedBrackets || matchPreviousCharacters(-1, "(", signOperators) || 
-				matchPreviousCharacters(-2, "(", ".")) {
-				return
-			}                 
-			outputID.textContent += operator
-			openedBrackets--
-			break
+	case "-":
+		// Allow minus sign to function as negative sign if last characters are not already sign operators
+		// console.log()
+		if(matchPreviousCharacters(-2, signOperators) ||
+			matchPreviousCharacters(-1, "-")) {
+			return
+		}
+		if (firstInput) outputID.textContent = operator
+		else outputID.textContent += operator
+		break 
 
-		default:
-			break
+	default:
+		// If previous character is open bracket or sign operator, then do not accept sign input. Prevents scenarios such as 27(*5) and 8*/2
+		if (matchPreviousCharacters(-1, "(", signOperators) || 
+			(firstInput && outputID.textContent == errorMessage)) {
+			return
+		}
+		outputID.textContent += operator
+		break
 	}
 
 	firstInput = false
+	decimalAllowed = true
 	this.blur(); // Removes focus from buttons
 }
 
-// Click event handler called for operators "(", ")", "Back", and "Clear"
-let operatorContainer = document.querySelectorAll(".operator")
-	operatorContainer.forEach(operator => {
-		operator.addEventListener('click', operatorFunc, false) 
-	})
+	
+function appendOperator(e) {
+let operator = e
 
-// Keyboard event handler called for operators "(", ")", "Back", and "Clear"
-window.addEventListener('keydown', operatorFunc, false)
+switch (operator) {
+	case "Back":
+		// If back, remove last element from output. Keeps track of opened brackets if there are any.
+		if (matchPreviousCharacters(-1, ")")) openedBrackets++
+		if (matchPreviousCharacters(-1, "(")) openedBrackets--
+		if (matchPreviousCharacters(-1, ".")) decimalAllowed = true
+		outputID.textContent = outputID.textContent.slice(0, -1)
+		break
+
+	case "Clear":
+		// If clear, erase display and allow decimals to be used
+		outputID.textContent = "0"
+		firstInput = true
+		decimalAllowed = true
+		openedBrackets = 0
+		return
+
+	case "(":
+		// If open bracket, increment bracket counter
+		if (firstInput) outputID.textContent = operator
+		else outputID.textContent += operator
+		openedBrackets++
+		break
+
+	case ")":
+		// If close bracket, make sure open bracket, sign operator or open bracket/decimal does not immediately preceed it. Avoids scenarios like "()", "45+(2+)", and "(.)"
+		if (!openedBrackets || matchPreviousCharacters(-1, "(", signOperators) || 
+			matchPreviousCharacters(-2, "(", ".")) {
+			return
+		}                 
+		outputID.textContent += operator
+		openedBrackets--
+		break
+
+	default:
+		break
+}
+
+firstInput = false
+this.blur(); // Removes focus from buttons
+}
+
+
 
 // Pass arithmetic expression as string to calc result. Using Regex to find/replace necessary terms. Could have used event handler logic, but wanted to practice some Regex black magic
 function operate(str) {
